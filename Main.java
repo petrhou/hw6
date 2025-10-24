87801f7cf8f00a4d800469087e322a88b4d732c4
Королев Пётр Алексеевич ИП - 516 <korolevp712@gmail.com>
2025-10-24 12:19:41 +0700

    добавлен метод getCarByBrendAndYearOperational

--- Main.java
+++ Main.java
@@ -1,41 +1,23 @@
-public class Main {
+public static Car[] getCarByBrendAndYearOperational(Car[] cars, String brend, int years) {
+    int currentYear = java.time.Year.now().getValue();
+    int count = 0;
     
-    public static Car[] getCarByBrend(Car[] cars, String brend) {
-        int count = 0;
-        
-        for (Car car : cars) {
-            if (car.getBrand().equalsIgnoreCase(brend)) {
-                count++;
-            }
+    for (Car car : cars) {
+        int yearsOperational = currentYear - car.getYear();
+        if (car.getBrand().equalsIgnoreCase(brend) && yearsOperational > years) {
+            count++;
         }
-        
-        Car[] result = new Car[count];
-        int index = 0;
-        
-  
-        for (Car car : cars) {
-            if (car.getBrand().equalsIgnoreCase(brend)) {
-                result[index++] = car;
-            }
-        }
-        
-        return result;
     }
     
-    public static void main(String[] args) {
-
-        Car[] cars = {
-            new Car(1, "Toyota", "Camry", 2020, "Black", 25000, "AB123CD"),
-            new Car(2, "Honda", "Civic", 2019, "White", 22000, "EF456GH"),
-            new Car(3, "Toyota", "Corolla", 2021, "Red", 20000, "IJ789KL"),
-            new Car(4, "BMW", "X5", 2018, "Blue", 45000, "MN012OP");
-        }
-        
-        Car[] toyotaCars = getCarByBrend(cars, "Toyota");
-        System.out.println("Найдено Toyota: " + toyotaCars.length);
-        
-        for (Car car : toyotaCars) {
-            System.out.println(car.getBrand() + " " + car.getModel());
+    Car[] result = new Car[count];
+    int index = 0;
+    
+    for (Car car : cars) {
+        int yearsOperational = currentYear - car.getYear();
+        if (car.getBrand().equalsIgnoreCase(brend) && yearsOperational > years) {
+            result[index++] = car;
         }
     }
+    
+    return result;
 }
