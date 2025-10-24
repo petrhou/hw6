015d7cb993c4d186db8847c8df30ad1efe73818a
Королев Пётр Алексеевич ИП - 516 <korolevp712@gmail.com>
2025-10-24 12:29:11 +0700

    test metod

--- Main.java
+++ Main.java
@@ -1,23 +1,22 @@
-public static Car[] getCarByBrendAndYearOperational(Car[] cars, String brend, int years) {
-    int currentYear = java.time.Year.now().getValue();
-    int count = 0;
+public static void main(String[] args) {
+    // Создаем массив машин для тестирования
+    Car[] cars = {
+        new Car(1, "Toyota", "Camry", 2020, "Black", 25000, "AB123CD"),
+        new Car(2, "Honda", "Civic", 2019, "White", 22000, "EF456GH"),
+        new Car(3, "Toyota", "Corolla", 2021, "Red", 20000, "IJ789KL"),
+        new Car(4, "BMW", "X5", 2018, "Blue", 45000, "MN012OP"),
+        new Car(5, "Toyota", "RAV4", 2015, "Silver", 18000, "QR345ST")
+    };
     
-    for (Car car : cars) {
-        int yearsOperational = currentYear - car.getYear();
-        if (car.getBrand().equalsIgnoreCase(brend) && yearsOperational > years) {
-            count++;
-        }
+    // Тестируем метод getCarByBrend
+    Car[] toyotaCars = getCarByBrend(cars, "Toyota");
+    System.out.println("Найдено Toyota: " + toyotaCars.length);
+    
+    // Тестируем метод getCarByBrendAndYearOperational
+    Car[] oldToyotaCars = getCarByBrendAndYearOperational(cars, "Toyota", 5);
+    System.out.println("Toyota старше 5 лет: " + oldToyotaCars.length);
+    
+    for (Car car : oldToyotaCars) {
+        System.out.println(car.getBrand() + " " + car.getModel() + " " + car.getYear() + " года");
     }
-    
-    Car[] result = new Car[count];
-    int index = 0;
-    
-    for (Car car : cars) {
-        int yearsOperational = currentYear - car.getYear();
-        if (car.getBrand().equalsIgnoreCase(brend) && yearsOperational > years) {
-            result[index++] = car;
-        }
-    }
-    
-    return result;
 }
